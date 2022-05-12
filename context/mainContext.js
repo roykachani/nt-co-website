3a71473ecb050fe2cfc7e18221f70f28902835fc
Roy Kachani <roy.kachani@gmail.com>
2022-05-12 19:09:43 -0300

    context, manejo de responsive tamaño de browser y carga de fuentes

--- context/mainContext.js
+++ context/mainContext.js
@@ -1,5 +1,7 @@
 import { useState, createContext, useEffect } from 'react';
 
+import { debounce } from '../utils/debounce';
+
 export const MainContext = createContext();
 
 const { Provider } = MainContext;
@@ -7,14 +9,37 @@ const { Provider } = MainContext;
 export const MainProvider = ({ children }) => {
 	const [mainState, setMainState] = useState({});
 	const [fontLoaded, setFontLoaded] = useState(false);
+	const [windowSize, setWindowSize] = useState({
+		width: undefined,
+		height: undefined,
+	});
 
 	useEffect(() => {
 		console.log(document.fonts);
 		document.fonts.ready.then(setFontLoaded(true));
+
+		//recalcula window size
+		const handleResize = () => {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+		};
+		handleResize();
+		//controla la cantidad de veces que el "user" hace resize para que no se repita el handleResize continuamente
+		const debouncedHandleResize = debounce(handleResize, 200);
+
+		window.addEventListener('resize', debouncedHandleResize);
+
+		return () => window.removeEventListener('resize', debouncedHandleResize);
 	}, []);
 	useEffect(() => {
 		console.log(document.fonts);
 	}, [fontLoaded]);
 
-	return <Provider value={{ mainState, fontLoaded }}>{children}</Provider>;
+	return (
+		<Provider value={{ mainState, fontLoaded, windowSize }}>
+			{children}
+		</Provider>
+	);
 };
