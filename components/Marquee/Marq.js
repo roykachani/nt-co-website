5ecb9eee7a9cf2498ab1f6ce755af17402608902
Roy Kachani <roy.kachani@gmail.com>
2022-05-31 11:57:41 -0300

    styles

--- components/Marquee/Marq.js
+++ components/Marquee/Marq.js
@@ -7,21 +7,21 @@ const Marq = () => {
 		<>
 			<div className={styles.main_container}>
 				<div className={styles.marquee_container}>
-					<div className={styles.marquee_text}>
-						<h2 className={styles.marquee_title}>
-							<span className={styles.marquee_span}>
-								Our Team Members were part of these rosters:
-							</span>
-						</h2>
-					</div>
 					<div className={styles.marquee_members_box}>
 						<div className={styles.overlay}>
 							<Marquee
 								pauseOnHover={true}
 								gradient={false}
 								className={styles.marquee_content}
-								style={{ width: '98%', height: '100%' }}
+								style={{ width: '100%', height: '100%' }}
 							>
+								<div className={styles.marquee_text}>
+									<h2 className={styles.marquee_title}>
+										<span className={styles.marquee_span}>
+											Our Team Members were part of these rosters:
+										</span>
+									</h2>
+								</div>
 								<div className={styles.marquee}>Member 1</div>
 								<div className={styles.marquee}>Member 2</div>
 								<div className={styles.marquee}>Member 3</div>
