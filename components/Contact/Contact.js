cd6c070b3c6ab187d7234030e91bbd97a4be6e0f
Roy Kachani <roy.kachani@gmail.com>
2022-06-09 11:26:04 -0300

    se quito un contacto

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -33,33 +33,25 @@ const Contact = ({ text, info }) => {
   // copy to clipboard
   // usar componente TEXTPRINT para imprimir texto clipboard
 
-  const showClipText = (e) => {
-    let clipText = e.target.id;
-    let type = clipText.split('-')[1];
-    if (type === 'A') {
-      textRefA.current.innerHTML = 'Copied to clipboard';
-      textRefA.current.classList.add(styles.copiedLink);
-      boxRefA.current.classList.add(styles.copiedLink);
-    } else {
-      textRefB.current.innerHTML = 'Copied to clipboard';
-      textRefB.current.classList.add(styles.copiedLink);
-      boxRefB.current.classList.add(styles.copiedLink);
-    }
+  const showClipText = () => {
+    textRefA.current.innerHTML = 'Copied to clipboard';
+    textRefA.current.classList.add(styles.copiedLink);
+    boxRefA.current.classList.add(styles.copiedLink);
     return setCopied(true);
   };
 
   useEffect(() => {
-    // console.log(copied, 'copied');
     if (!!copied) {
       setTimeout(() => {
-        // console.log(copied, 'time out');
         textRefA.current.classList.remove(styles.copiedLink);
-        textRefB.current.classList.remove(styles.copiedLink);
         boxRefA.current.classList.remove(styles.copiedLink);
-        boxRefB.current.classList.remove(styles.copiedLink);
         setCopied(false);
       }, 2000);
     }
+    if (!copied)
+      setTimeout(() => {
+        textRefA.current.innerHTML = '';
+      }, 1000);
     return clearTimeout();
   }, [copied]);
 
@@ -90,46 +82,13 @@ const Contact = ({ text, info }) => {
                     className={styles.iconCopy} //styles padre
                     ref={boxRefA}
                     id="copy-A"
-                    onClick={(e) => {
+                    onClick={() => {
                       navigator.clipboard.writeText(info);
-                      showClipText(e);
+                      showClipText();
                     }}
                   >
                     <span
                       ref={textRefA}
-                      hidden={!copied}
-                      className={styles.notCopiedLink} //styles hijo text
-                    ></span>
-                    <CopiedLink />
-                  </div>
-                </h4>
-              </div>
-            </div>
-            <div className={styles.contact_box_right}>
-              <div className={styles.contact_figure}>
-                <div className={styles.contact_figure_diamond}>
-                  <div className={styles.contact_figure_diamond_inner}></div>
-                </div>
-              </div>
-              <div className={styles.contact_email}>
-                <h4 className={styles.email}>
-                  <a
-                  // href={`mailto:${info}`}
-                  >
-                    {info}
-                  </a>
-                  <div
-                    className={styles.iconCopy} //styles padre
-                    id="copy-B"
-                    ref={boxRefB}
-                    onClick={(e) => {
-                      navigator.clipboard.writeText(info);
-                      showClipText(e);
-                    }}
-                  >
-                    <span
-                      ref={textRefB}
-                      hidden={!copied}
                       className={styles.notCopiedLink} //styles hijo text
                     ></span>
                     <CopiedLink />
