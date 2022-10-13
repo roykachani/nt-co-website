5cef4dcda57c94260d205b20eb04f4b52147d011
Roy Kachani <roy.kachani@gmail.com>
2022-10-13 15:58:43 -0300

    nueva vista interna

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -22,20 +22,41 @@ const Card = ({ skill }) => {
         >
           <div className={`${styles.card_face} ${styles.card_face_front}`}>
             <div className={styles.card_figure}>
-              <div>
-                <Image src={svgPath} width={243} height={320}></Image>
-              </div>
+              {/* <div> */}
+              <Image
+                src={svgPath}
+                width={243}
+                height={360}
+                alt={name}
+                objectFit="cover"
+                loading="eager"
+              />
+              {/* </div> */}
             </div>
           </div>
           <div className={`${styles.card_face} ${styles.card_face_back}`}>
-            <div className={styles.card_content}>
-              {description.map((item, index) => {
-                return (
-                  <div key={skill.id + index}>
-                    <h4 className={styles.content_description}>{item}</h4>
-                  </div>
-                );
-              })}
+            <div
+              className={`${styles.card_figure} ${styles.back_figure_opacity}`}
+            >
+              <Image
+                src={svgPath}
+                width={243}
+                height={360}
+                alt={name}
+                objectFit="cover"
+                loading="eager"
+              />
+            </div>
+            <div className={styles.card_content_blur}>
+              <div className={styles.card_content}>
+                {description.map((item, index) => {
+                  return (
+                    <div key={skill.id + index}>
+                      <h4 className={styles.content_description}>{item}</h4>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
