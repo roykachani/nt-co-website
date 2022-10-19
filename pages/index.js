a4a6e66dcfff553c7fea562736fa2fa4d6d691f1
Roy Kachani <roy.kachani@gmail.com>
2022-10-19 11:25:32 -0300

    seo

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,21 @@ export default function Home({ data }) {
   return (
     <>
       <Head>
+        <meta charset="utf-8" />
         <title>NT Co. | digital agency</title>
         <meta name="description" content={info.description} />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="keywords"
+          content="end-to-end agency, digital agency, nice try, nice try co., new trends and develop, help organizations level up their brands, Digital Marketing, strategy, Brand Strategy, Planning, Research, gaming, Team Management, Events, Tournaments, Events & Tournaments, Live Streams, design, Branding, Product Development, Motion,UX / UI, App Development, Content Production, Live Streams, Audiovisual"
+        />
+        <link rel="canonical" href="https://nice-try.co/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta content="#18181a" name="theme-color" />
+        <meta name="copyright" content={info.copyright} />
+        <meta name="author" content={info.author} />
       </Head>
 
       <main>
