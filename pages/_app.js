d8f43dd4d5f0158fb15766fdff879e3c22d1de08
Roy Kachani <roy.kachani@gmail.com>
2022-10-20 17:08:40 -0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,29 @@
+import Script from 'next/script';
+
 import { MainProvider } from '../context/mainContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<MainProvider>
-			<Component {...pageProps} />
-		</MainProvider>
-	);
+  return (
+    <>
+      <MainProvider>
+        <Component {...pageProps} />
+      </MainProvider>
+
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-7TYHQKNE88"
+      />
+      <Script strategy="afterInteractive" id="google-analytics">
+        {`
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'G-7TYHQKNE88');
+		`}
+      </Script>
+    </>
+  );
 }
 
 export default MyApp;
